73455238101baf99302851497c32200df1dcc4ad
Mikhail Svetlov <jegnum@gmail.com>
2022-08-16 16:52:24 +0300

    add gitignore

--- SurfSummerSchoolProject/AppDelegate.swift
+++ SurfSummerSchoolProject/AppDelegate.swift
@@ -5,10 +5,34 @@
 //  Created by Михаил Светлов on 02.08.2022.
 //
 
+import CoreData
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+//    lazy var persistentContainer: NSPersistentContainer = {
+//        let container = NSPersistentContainer(name: "CoreData")
+//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+//            if let error = error as NSError? {
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
+//            }
+//        })
+//        return container
+//    }()
+    
+//    func saveContext() {
+//        let context = persistentContainer.viewContext
+//        if context.hasChanges {
+//            do {
+//                try context.save()
+//            } catch {
+//                let nserror = error as NSError
+//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//            }
+//        }
+//    }
 
     var window: UIWindow?
     var tokenStorage: TokenStorage {
@@ -59,5 +83,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window?.rootViewController = lauchScreenViewController
     }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        StorageManager.shared.saveContext()
+        }
 
 }
---
+++ SurfSummerSchoolProject/Services/StorageManager/CoreDataStorageManager.swift
@@ -0,0 +1,46 @@
+//
+//  CoreDataStorageManager.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Михаил Светлов on 14.08.2022.
+//
+
+import Foundation
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreData")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+
+
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    // MARK: - CRUD
+
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
